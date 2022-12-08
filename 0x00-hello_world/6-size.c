d84fbf7810444c308da075a0dde4d47a3b124b72
Nzomo <brnzomo17@gmail.com>
2022-12-08 15:39:52 -0800

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return : Always 0
+ * main - Entry point
+ *
+ * Return : Always 0 (success)
  */
+
 int main(void)
 {
 	printf("Size of a char : %lu byte(s)\n", sizeof(char));
