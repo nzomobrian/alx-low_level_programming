bce8639f01daaf455e570326922323bf34fd1477
Nzomo <brnzomo17@gmail.com>
2022-12-15 15:59:50 +0300

    More functions,nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,22 +9,23 @@
  */
 int main(void)
 {
-	int num;
+        int num;
 
-	for (num = 1; num <= 100; num++)
-	{
-		if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
+        for (num = 1; num <= 100; num++)
+        {
+                if ((num % 3) == 0 && (num % 5) == 0)
+                        printf("FizzBuzz");
 
-		else if ((num % 3) == 0)
-			printf("Fizz");
+                else if ((num % 3) == 0)
+                        printf("Fizz");
 
-		else if ((num % 5) == 0)
-			printf("Buzz");
+                else if ((num % 5) == 0)
+                        printf("Buzz");
 
-		if (num == 100)
-			continue;
-		printf(" ");
-	}
-	return (0);
+                if (num == 100)
+                        continue;
+                printf(" ");
+        }
+        return (0);
 }
+
