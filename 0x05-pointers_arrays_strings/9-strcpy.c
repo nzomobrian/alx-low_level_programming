a64f051aab797f104adee1a20b2ffaa7270549e9
Nzomo <brnzomo17@gmail.com>
2022-12-19 22:40:02 +0300

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	char *d = dest;
 
-	for (; src[i] != '\0'; i++)
+	for (; *src != '\0'; src++)
 	{
-		dest[i] = src[i];
+		*dest = *src;
+		dest++;
 	}
-	return (dest);
+	*dest = *src;
+	return (d);
 }
