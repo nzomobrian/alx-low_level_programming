0a640004e80ce1a06496b0f852a449c5d949c435
Nzomo <brnzomo17@gmail.com>
2022-12-23 12:58:37 +0300

    pointers,arrays,strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,18 +6,17 @@
 */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int num = n;
 
-	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		num = -num;
 	}
-{
-	print_number(n1 / 10);
-}
 
-	_putchar((n1 % 10) + '0');
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
 
